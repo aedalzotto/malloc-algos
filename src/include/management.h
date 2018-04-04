e722b4ee86c77828b0000625d32d09940cdf85c6
Gabriel Boni Vicari <gabrielbvicari@gmail.com>
2018-04-04 18:08:18 -0300

    added mmalgo_rithm class

--- src/include/management.h
+++ src/include/management.h
@@ -1,2 +1,12 @@
 #include <iostream>
 
+class mmalgo_rithm {
+public:
+    mmalgo_rithm();
+    void mmalgo_first_fit();
+    void mmalgo_next_fit();
+    void mmalgo_best_fit();
+    void mmalgo_worst_fit();
+
+private:
+};
--- src/management.cpp
+++ src/management.cpp
@@ -1,1 +1,21 @@
 #include "management.h"
+
+mmalgo_rithm::mmalgo_rithm(){
+
+}
+
+void mmalgo_first_fit(){
+
+}
+
+void mmalgo_best_fit(){
+    
+}
+
+void mmalgo_best_fit(){
+    
+}
+
+void mmalgo_worst_fit(){
+    
+}
