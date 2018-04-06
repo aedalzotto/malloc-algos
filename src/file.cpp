8df1b3dc5028567bfd1f48272d1925e821f58acf
Angelo Elias Dalzotto <angelodalzotto97@gmail.com>
2018-04-05 21:03:24 -0300

    file parser updated

--- src/file.cpp
+++ src/file.cpp
@@ -1,4 +1,5 @@
 #include "file.h"
+#include <sstream>
 
 mmalgo_parser::mmalgo_parser()
 {
@@ -7,20 +8,138 @@ mmalgo_parser::mmalgo_parser()
 
 void mmalgo_parser::open_list(std::string file_name)
 {
-    try{
-        list_file.open(file_name);
-    } catch(...){
-        throw;
+    memory.clear();
+    std::fstream file;
+
+    file.open(file_name, std::fstream::in);
+    if(!file.is_open())
+        throw std::runtime_error("Unable to open list.");
+
+    std::string buffer;
+    std::vector<std::string> buffer_split;
+    std::vector<unsigned int> inputs;
+    while(!file.eof()){
+        std::getline(file, buffer);
+        if(!buffer.size())
+            break;
+
+        buffer_split.clear();
+        buffer_split = explode(buffer, ' ');
+        if(buffer_split.size() != 2){
+            file.close();
+            throw std::runtime_error("Número de colunas inválido.");
+        }
+
+        //Evita ler header
+        inputs.clear();
+        unsigned int value;
+        for(auto& s : buffer_split){
+            if(isInteger(s)){
+                std::istringstream(s) >> value;
+                inputs.push_back(value);
+            }else{
+                inputs.clear();
+                break;
+            }
+        }
+
+        if(inputs.size() == 2)
+            memory.push_back(mem_list(inputs[0], inputs[1]));
+
     }
-   
+    inputs.clear();
+    buffer_split.clear();
+    buffer.clear();
+    file.close();
 }
 
 void mmalgo_parser::open_task(std::string file_name)
 {
-    try{
-        task_file.open(file_name);
-    } catch(...){
-        throw;
-    }
+    task.clear();
+    std::fstream file;
 
+    file.open(file_name, std::fstream::in);
+    if(!file.is_open())
+        throw std::runtime_error("Unable to open tasks.");
+
+    std::string buffer;
+    std::vector<std::string> buffer_split;
+    std::vector<unsigned int> inputs;
+    while(!file.eof()){
+        std::getline(file, buffer);
+        if(!buffer.size())
+            break;
+
+        buffer_split.clear();
+        buffer_split = explode(buffer, ' ');
+        if(buffer_split.size() != 2){
+            file.close();
+            throw std::runtime_error("Número de colunas inválido.");
+        }
+
+        //Evita ler header
+        inputs.clear();
+        unsigned int value;
+        for(auto& s : buffer_split){
+            if(isInteger(s)){
+                std::istringstream(s) >> value;
+                inputs.push_back(value);
+            }else{
+                inputs.clear();
+                break;
+            }
+        }
+
+        if(inputs.size() == 2)
+            task.push_back(jobs(inputs[0], inputs[1]));
+
+    }
+    inputs.clear();
+    buffer_split.clear();
+    buffer.clear();
+    file.close();
+}
+
+const std::vector<std::string> mmalgo_parser::explode(const std::string& s, const char& c)
+{
+    std::string buff{""};
+    std::vector<std::string> v;
+
+    for(auto n:s)
+    {
+        if(n != c) buff+=n; else
+        if(n == c && buff != "") { v.push_back(buff); buff = ""; }
+    }
+    if(buff != "") v.push_back(buff);
+
+    return v;
+}
+
+bool mmalgo_parser::isInteger(const std::string &s)
+{
+    if(s.empty() || ((!isdigit(s[0])) && (s[0] != '-') && (s[0] != '+'))) return false ;
+
+    char * p ;
+    strtol(s.c_str(), &p, 10) ;
+
+    return (*p == 0);
+}
+
+mem_list::mem_list(unsigned int id, unsigned int size)
+{
+    this->id = id;
+    index.push_back(mem_index(size));
+}
+
+mem_index::mem_index(unsigned int size)
+{
+    is_hole = true;
+    start = 0;
+    this->size = size;
+}
+
+jobs::jobs(unsigned int id, unsigned int size)
+{
+    this->id = id;
+    this->size = size;
 }
--- src/include/file.h
+++ src/include/file.h
@@ -6,20 +6,42 @@
 #include <string>
 #include <fstream>
 
+class mem_index {
+public:
+    mem_index(unsigned int size);
+    bool is_hole;
+    unsigned int start;
+    unsigned int size;
+};
+
+class mem_list {
+public:
+    mem_list(unsigned int id, unsigned int size);
+    unsigned int id;
+    std::vector<mem_index> index;
+};
+
+class jobs {
+public:
+    jobs(unsigned int id, unsigned int size);
+    unsigned int id;
+    unsigned int size;
+};
+
 class mmalgo_parser {
 public:
     mmalgo_parser();
     void open_list(std::string file_name);
     void open_task(std::string file_name);
 
+    std::vector<mem_list> memory;
+    std::vector<jobs> task;
+
 private:
-    std::vector<int> list;
-    std::vector<int> task;
-    std::ifstream task_file;
-    std::ifstream list_file;
-
-    void open_file(std::string file_name);
-
+    const std::vector<std::string> explode(const std::string& s, const char& c);
+    bool isInteger(const std::string &s);
 };
+
+
 
 #endif
